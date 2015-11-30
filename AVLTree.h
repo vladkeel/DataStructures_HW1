d998b99b80c6882ae7793e56cf5c45333adf9621
Vlad Keel <Vlad Keel>
2015-11-30 15:49:11 +0200

    New AVL struct + empty constructor of size

--- AVLTree.h
+++ AVLTree.h
@@ -3,21 +3,35 @@
 #include <algorithm>
 #include "Exceptions.h"
 namespace AVL{
-	template <class T> class Node{
+	template <class N, class M>
+	static Node<N,M>* buildEmpty(int size){
+		if (size == 0){
+			return nullptr
+		}
+		else if (size == 1){
+			return new Node<N, M>();
+		}
+		Node<S, T>* newNode = new Node<N, M>();
+		newNode->left = buildEmpty(std::ceil((size - 1) / 2));
+		NewNode->right = buildEmpty(std::floor((size - 1) / 2));
+		return newNode;
+	}
+	template <class S, class T> class Node{
 	private:
-		template <class U> friend class Tree;
-		int key;
+		template <class F, class U> friend class Tree;
+		template <class N, class M> friend Node* buildEmpty;
+		S key;
 		T data;
 		int height;
-		Node<T>* left;
-		Node<T>* right;
-		Node();
+		Node<S, T>* left;
+		Node<S, T>* right;
+		Node() :left(0), right(0),key(),data(){};
 		void updateHeight(){
 			int heightLeft = left ? left->height : -1;
 			int heightRight = right ? right->height : -1;
 			height = std::max<int>(heightLeft, heightRight) + 1;
 		}
-		Node<T>* removeMin(){
+		Node<S, T>* removeMin(){
 			if (!left)
 				return right;
 			left = removeMin();
@@ -28,23 +42,23 @@ namespace AVL{
 			int heightRight = right ? right->height : -1;
 			return heightLeft - heightRight;
 		}
-		Node<T>* rotateRight(){
-			Node<T>* newRoot = left;
+		Node<S, T>* rotateRight(){
+			Node<S, T>* newRoot = left;
 			left = newRoot->right;
 			newRoot->right = this;
 			updateHeight();
 			newRoot->updateHeight();
 			return newRoot;
 		}
-		Node<T>* rotateLeft(){
-			Node<T>* newRoot = right;
+		Node<S, T>* rotateLeft(){
+			Node<S, T>* newRoot = right;
 			right = newRoot->left;
 			newRoot->left = this;
 			updateHeight();
 			newRoot->updateHeight();
 			return newRoot;
 		}
-		Node<T>* balance(){
+		Node<S, T>* balance(){
 			updateHeight();
 			if (balanceFactor() == 2)
 			{
@@ -61,10 +75,10 @@ namespace AVL{
 			return this; // no balance needed
 		}
 	protected:
-		Node<T>* findMin(){
+		Node<S, T>* findMin(){
 			return left ? left->findMin() : this;
 		}
-		Node<T>* findMax(){
+		Node<S, T>* findMax(){
 			return right ? right->findMax() : this;
 		}
 		void killNode(){
@@ -74,8 +88,8 @@ namespace AVL{
 				right->killNode();
 			delete this;
 		}
-		Node<T>* copyNode(){
-			Node<T>* newNode = new Node<T>(*this);
+		Node<S, T>* copyNode(){
+			Node<S, T>* newNode = new Node<S, T>(*this);
 			newNode->key = key;
 			newNode->data = data;
 			newNode->height = height;
@@ -88,12 +102,12 @@ namespace AVL{
 			return newNode;
 		}
 	public:
-		Node(int key, const T& data) :key(key), data(data), left(0), right(0), height(0){};
-		Node(const Node<T>& copyN) :key(copyN.key), data(copyN.data), height(copyN.height){
+		Node(S& key, const T& data) :key(key), data(data), left(0), right(0), height(0){};
+		Node(const Node<S, T>& copyN) :key(copyN.key), data(copyN.data), height(copyN.height){
 			right = copyN.right ? copyN.right->copyNode() : 0;
 			left = copyN.left ? copyN.left->copyNode() : 0;
 		};
-		Node<T>& operator=(const Node<T>& copyN){
+		Node<S, T>& operator=(const Node<S, T>& copyN){
 			if (this != &copyN){
 				killNode();
 				key = copyN.key;
@@ -104,7 +118,7 @@ namespace AVL{
 			}
 			return *this;
 		}
-		~Node<T>(){};
+		~Node<S, T>(){};
 		int getHeight()const{
 			return height;
 		}
@@ -114,26 +128,25 @@ namespace AVL{
 		T& getData()const{
 			return data;
 		}
-		Node* insert(int k, const T& data) {
-			if (k < key)
+		Node* insert(S& k, const T& data) {
+			if (k < key){
 				if (!left)
 					left = new Node(k, data);
 				else
 					left = left->insert(k, data);
-			else if (k > key)
+			} else if (k == key)
+				throw AlreadyExists();
+			else{
 				if (!right)
 					right = new Node(k, data);
 				else
 					right = right->insert(k, data);
+			}
 			return balance();
 		}
-		Node* remove(int k){
+		Node* remove(S& k){
 			if (k < key && left){
 				left = left->remove(k);
-				return balance();
-			}
-			else if (k > key && right){
-				right = right->remove(k);
 				return balance();
 			}
 			else if (k == key){
@@ -146,10 +159,14 @@ namespace AVL{
 				min->left = leftTemp;
 				return min->balance();
 			}
+			else if (right){
+				right = right->remove(k);
+				return balance();
+			}
 			else
 				throw ElementNotFound();
 		}
-		Node* find(int k){
+		Node* find(S& k){
 			if (k == key)
 				return this;
 			if (k > key && right)
@@ -173,14 +190,17 @@ namespace AVL{
 				left->reverseInorderScan(action);
 		}
 	};
-	template <class U> class Tree{
+	template <class F,class U> class Tree{
 	private:
 		int size;
 	public:
-		Node<U>* root;
+		Node<F,U>* root;
 		Tree() : root(0),size(0){};
-		Tree(int key, const U& data):size(1){
-			root = new Node<U>(key, data);
+		Tree(int size):size(size){
+			root = buildEmpty<F,U>(size);
+		}
+		Tree(F& key, const U& data):size(1){
+			root = new Node<F,U>(key, data);
 		}
 		Tree(const Tree& tree){
 			root = tree.root? tree.root->copyNode() : 0;
@@ -202,21 +222,21 @@ namespace AVL{
 		int getSize(){
 			return size;
 		}
-		void insert(int key, const U& data){
+		void insert(F& key, const U& data){
 			root = root ? root->insert(key, data) : new Node<U>(key,data);
 			++size;
 		}
-		void remove(int key){
+		void remove(F& key){
 			root = root ? root->remove(key) : 0;
 			--size;
 		}
-		Node<U>* find(int key){
+		Node<F,U>* find(F& key){
 			return root ? root->find(key) : 0;
 		}
-		Node<U>* findMin(){
+		Node<F,U>* findMin(){
 			return root ? root->findMin() : 0;
 		}
-		Node<U>* findMax(){
+		Node<F,U>* findMax(){
 			return root ? root->findMax() : 0;
 		}
 		template <typename actionT> void inorderScan(actionT action)const{
