52f44a6c8f0bce36aa09f208dfd81a26a73024da
Vlad Keel <Vlad Keel>
2015-12-01 10:55:16 +0200

    Add base exception and implement library1

--- Exceptions.h
+++ Exceptions.h
@@ -7,14 +7,46 @@
 
 #ifndef EXCEPTIONS_H_
 #define EXCEPTIONS_H_
+typedef enum {
+	SUCCESS = 0,
+	FAILURE = -1,
+	ALLOCATION_ERROR = -2,
+	INVALID_INPUT = -3
+} ExceptionStatusType;
+class BaseException : public std::exception{
+private:
+	ExceptionStatusType type;
+public:
+	BaseException() :exception(){};
+	BaseException(ExceptionStatusType type) :exception(), type(type){};
+	ExceptionStatusType getType()const{
+		return type;
+	}
+};
+class IsEmptyException : public BaseException{
+public:
+	IsEmptyException() :BaseException(FAILURE){};
+};
+class InvalidInput : public BaseException{
+public:
+	InvalidInput() :BaseException(INVALID_INPUT){};
+};
+class ElementNotFound : public BaseException {
+public:
+	ElementNotFound() :BaseException(FAILURE){};
+};
+class AlreadyExists : public BaseException {
+public:
+	AlreadyExists() :BaseException(FAILURE){};
+};
+class DoesntExist : public BaseException {
+public:
+	DoesntExist() : BaseException(FAILURE){};
+};
 
-class IsEmptyException : public std::exception{};
-class InvalidInput : public std::exception{};
-class ElementNotFound : public std::exception {} ;
-class AlreadyExists : public std::exception {} ;
-class DoesntExist : public std::exception {} ;
-
-class Failure : public std::exception {} ;
-
+class Failure : public BaseException {
+public:
+	Failure() : BaseException(FAILURE){};
+};
 
 #endif /* EXCEPTIONS_H_ */
--- library1.cpp
+++ library1.cpp
@@ -1,1 +1,134 @@
 //Here be dragons
+#include "library1.h"
+#include "Pokedex.h"
+using namespace Pokedex;
+void* Init(){
+	try{
+		PokedexDS* DS = new PokedexDS();
+		return DS;
+	}
+	catch (std::bad_alloc& e){
+		return 0;
+	}
+}
+StatusType AddTrainer(void *DS, int trainerID){
+	if (!DS){
+		return (StatusType)INVALID_INPUT;
+	}
+	try{
+		((PokedexDS*)DS)->addTrainer(trainerID);
+	}
+	catch (BaseException& e){
+		return (StatusType)e.getType();
+	}
+	catch (std::bad_alloc& e){
+		return (StatusType)ALLOCATION_ERROR;
+	}
+
+}
+StatusType CatchPokemon(void *DS, int pokemonID, int trainerID, int level){
+	if (!DS){
+		return (StatusType)INVALID_INPUT;
+	}
+	try{
+		((PokedexDS*)DS)->catchPokemon(pokemonID, trainerID, level);
+	}
+	catch (BaseException& e){
+		return (StatusType)e.getType();
+	}
+	catch (std::bad_alloc& e){
+		return (StatusType)ALLOCATION_ERROR;
+	}
+}
+StatusType FreePokemon(void *DS, int pokemonID){
+	if (!DS){
+		return (StatusType)INVALID_INPUT;
+	}
+	try{
+		((PokedexDS*)DS)->freePokemon(pokemonID);
+	}
+	catch (BaseException& e){
+		return (StatusType)e.getType();
+	}
+	catch (std::bad_alloc& e){
+		return (StatusType)ALLOCATION_ERROR;
+	}
+}
+StatusType LevelUp(void *DS, int pokemonID, int levelIncrease){
+	if (!DS){
+		return (StatusType)INVALID_INPUT;
+	}
+	try{
+		((PokedexDS*)DS)->levelUp(pokemonID, levelIncrease);
+	}
+	catch (BaseException& e){
+		return (StatusType)e.getType();
+	}
+	catch (std::bad_alloc& e){
+		return (StatusType)ALLOCATION_ERROR;
+	}
+}
+StatusType EvolvePokemon(void *DS, int pokemonID, int evolvedID){
+	if (!DS){
+		return (StatusType)INVALID_INPUT;
+	}
+
+	try{
+		((PokedexDS*)DS)->evolvePokemon(pokemonID,evolvedID);
+	}
+	catch (BaseException& e){
+		return (StatusType)e.getType();
+	}
+	catch (std::bad_alloc& e){
+		return (StatusType)ALLOCATION_ERROR;
+	}
+}
+StatusType GetTopPokemon(void *DS, int trainerID, int *pokemonID){
+	if (!DS){
+		return (StatusType)INVALID_INPUT;
+	}
+	try{
+		*pokemonID = ((PokedexDS*)DS)->getTopPokemon(trainerID);
+	}
+	catch (BaseException& e){
+		return (StatusType)e.getType();
+	}
+	catch (std::bad_alloc& e){
+		return (StatusType)ALLOCATION_ERROR;
+	}
+}
+StatusType GetAllPokemonsByLevel(void *DS, int trainerID, int **pokemons, int *numOfPokemon){
+	if (!DS){
+		return (StatusType)INVALID_INPUT;
+	}
+	try{
+		*pokemons = ((PokedexDS*)DS)->GetAllPokemonsByLevel(trainerID,numOfPokemon);
+	}
+	catch (BaseException& e){
+		return (StatusType)e.getType();
+	}
+	catch (std::bad_alloc& e){
+		return (StatusType)ALLOCATION_ERROR;
+	}
+}
+StatusType UpdateLevels(void *DS, int stoneCode, int stoneFactor){
+	if (!DS){
+		return (StatusType)INVALID_INPUT;
+	}
+
+	try{
+		((PokedexDS*)DS)->UpdateLevels(stoneCode, stoneFactor);
+	}
+	catch (BaseException& e){
+		return (StatusType)e.getType();
+	}
+	catch (std::bad_alloc& e){
+		return (StatusType)ALLOCATION_ERROR;
+	}
+}
+void Quit(void** DS){
+	if (!DS || !(*DS)){
+		return;
+	}
+	delete *DS;
+}
